7246a6e6ef6299d90ae607d1562e9523a1321eb8
Juan <Juan@ds.co>
2016-11-02 19:41:22 -0300

    sd

--- while_ut1Obligatorio/src/examples/while_ut1/ast/CheckStateLinter.java
+++ while_ut1Obligatorio/src/examples/while_ut1/ast/CheckStateLinter.java
@@ -1,12 +1,14 @@
 package examples.while_ut1.ast;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CheckStateLinter {
 
 	
 	public ArrayList<String> errores=new ArrayList<String>();
-	
+	public Map <String,ObjectState>mapa=new HashMap<String,ObjectState>(); //nombre varible definida no valor
 	
 	
 	
